e0fdf73ad2ceb66a40e7bc820e0a5ff3e14b20c1
Pedro Trindade <pedrotrindade@Pedros-MacBook-Pro.local>
2020-04-08 18:06:01 -0500

    Implement api to get post summaries

--- src/containers/Home/HomeScreen.tsx
+++ src/containers/Home/HomeScreen.tsx
@@ -1,38 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@material-ui/core";
+import axios from "axios";
 
 import PostSummary from "../../models/PostSummary";
 
 import Outdoor from "../../components/Outdoor/Outdoor";
 import PostList from "../../components/PostList/PostList";
 
-const postList: PostSummary[] = [
-  {
-    id: "dqE7u6Cm7NYRotmjCfRc",
-    title: "Como eu saí completamente da minha zona de conforto",
-    author: "Pedro Trindade",
-    content:
-      "Eu posso me lembrar perfeitamente de um dia, talvez dois anos atrás, quando eu estava um pouco desmotivado pensando sobre a vida que uma pessoa que vive no meu país de origem precisa ter para buscar o sucesso.",
-    languages: ["pt-BR", "en-US"],
-    datetime: new Date("2020-02-27T17:00:00.000Z"),
-  },
-  {
-    id: "5as1d51as2d1asd1",
-    title: "Como eu saí completamente da minha zona de conforto",
-    author: "Pedro Trindade",
-    content:
-      "Eu posso me lembrar perfeitamente de um dia, talvez dois anos atrás, quando eu estava um pouco desmotivado pensando sobre a vida que uma pessoa que vive no meu país de origem precisa ter para buscar o sucesso.",
-    languages: ["pt-BR", "en-US"],
-    datetime: new Date("2020-02-27T17:00:00.000Z"),
-  },
-];
-
 export default function HomeScreen() {
+  const [state, setState] = useState({
+    posts: [] as PostSummary[],
+    loading: true,
+  });
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5003/cultivando-ideias/us-central1/api/posts/summary/pt-BR`
+      )
+      .then((response) => {
+        response.data.forEach((x: PostSummary) => {
+          x.datetime = new Date(x.datetime);
+        });
+        setState(() => ({ posts: response.data, loading: false }));
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <React.Fragment>
       <Outdoor />
       <Container maxWidth="sm">
-        <PostList posts={postList} />
+        <PostList posts={state.posts} />
       </Container>
     </React.Fragment>
   );
