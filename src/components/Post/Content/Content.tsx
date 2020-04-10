47e57c9d3510d03da04417145c6e2278d7a2edfc
Pedro Trindade <pedrotrindade@Pedros-MacBook-Pro.local>
2020-04-09 20:48:12 -0500

    Create component to render formatted post content

---
+++ src/components/Post/Content/Content.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+import {
+  EditorState,
+  convertToRaw,
+  ContentState,
+  convertFromRaw,
+} from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
+
+import "./Content.css";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      fontFamily: "'Inria Sans'",
+    },
+  })
+);
+
+type ContentProps = {
+  content: string;
+};
+
+const Content: FunctionComponent<ContentProps> = ({ content }) => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={classes.root}
+      dangerouslySetInnerHTML={{ __html: draftToHtml(JSON.parse(content)) }}
+    ></div>
+  );
+};
+
+export default Content;
